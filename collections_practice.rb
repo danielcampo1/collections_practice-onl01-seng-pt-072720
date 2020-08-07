ca11f294c27dfabe9ad0f998e85b625376be60da
daniel campo <campo_daniel94@yahoo.com>
2020-08-07 20:42:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,13 +40,7 @@ def find_a(array)
   end
   new_array
  end
- 
- 
-# ["Tim", "Tom", "Jim"].each do |name|
-#   if name.start_with?("T")
-#     puts "Hi, #{name}"
-#   end
-# end
+
 
 # sum_array
 # end
